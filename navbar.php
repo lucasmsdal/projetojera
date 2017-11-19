177c62d168acce4eb93bfed3fa3451f626ea2ae9
lucasmsdal <lucas.msdal@gmail.com>
2017-11-19 11:05:46 -0300

    Dividi o código inicial do bootstrap em três partes: cabeçalho, rodapé e navbar

---
+++ navbar.php
@@ -0,0 +1,18 @@
+	<body>
+    <nav class="navbar navbar-fixed-top navbar-inverse">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+          </button>
+          <a class="navbar-brand" href="#">Cadastro de Links</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav" "align center">
+            <li><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!-- /.nav-collapse -->
+      </div><!-- /.container -->
+    </nav><!-- /.navbar -->
