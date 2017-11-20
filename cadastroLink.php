0e26cc3bc67ab9b8d214878df736305693fd2521
lucasmsdal <lucas.msdal@gmail.com>
2017-11-20 13:45:48 -0300

    Correção no código sql relicionado a inserção dos links no banco de dados

--- cadastroLink.php
+++ cadastroLink.php
@@ -8,21 +8,20 @@ include("cabecalho.php");
    $titulo = $_POST['titulo'];
    $link = $_POST['link'];
  
- 
    
    include("conecta.php"); 
 
    
    function insereLink ($conexao, $titulo, $link){
     
-    $query = "insert into link (titulo, link) values ('{$titulo}', '{$linkl}')";
+    $query = "insert into cadastros (titulo, link) values ('{$titulo}', '{$link}')";
     $resultadoInsercao = mysqli_query($conexao, $query);
         
     
     return $resultadoInsercao;
 }
 
-if (insereLink ($conexao, $nome, $email, $senha)){
+if (insereLink ($conexao, $titulo, $link)){
           echo"<script language='javascript' type='text/javascript'>alert('Adicionado com Sucesso');window.location.href='index.php';</script>";
           die();
         }else{
