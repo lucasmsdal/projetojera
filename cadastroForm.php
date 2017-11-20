ef625e553f0cc0bba3c7b94ad44cd740cafa0816
lucasmsdal <lucas.msdal@gmail.com>
2017-11-19 22:11:33 -0300

    Contém a função para inserção do link ao banco de dados

---
+++ cadastroForm.php
@@ -0,0 +1,41 @@
+<?php
+include("cabecalho.php");
+
+?>
+<?php
+ 
+
+   $titulo = $_POST['titulo'];
+   $link = $_POST['link'];
+ 
+ 
+   
+   include("conecta.php"); 
+
+   
+   function insereLink ($conexao, $titulo, $link){
+    
+    $query = "insert into cadastros (titulo, link) values ('{$titulo}', '{$linkl}')";
+    $resultadoInsercao = mysqli_query($conexao, $query);
+        
+    
+    return $resultadoInsercao;
+}
+
+if (insereLink ($conexao, $titulo, $link)){
+          echo"<script language='javascript' type='text/javascript'>alert('Adicionado com Sucesso');window.location.href='index.php';</script>";
+          die();
+        }else{
+          
+           echo"<script language='javascript' type='text/javascript'>alert('Não foi adicionado com Sucesso');window.location.href='linkForm.php';</script>";
+          die();
+           
+        }
+         
+
+      
+?>
+
+      <?php
+include("rodape.php");
+?>
