9ebb5ee0577a8d0914baab8c5cbb1718de7672be
lucasmsdal <lucas.msdal@gmail.com>
2017-11-19 22:05:05 -0300

    Contém a função que seleciona os links salvos no banco de dados

---
+++ bancoLink.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+function listaLink($conexao){
+	mysqli_set_charset($conexao, "utf8");
+	$cadastros = array();
+	$resultado = mysqli_query($conexao, "select * from cadastros");
+	while($pergunta = mysqli_fetch_assoc($resultado)){
+		array_push($cadastros, $cadastro);
+	}
+	return $cadastros;
+}
