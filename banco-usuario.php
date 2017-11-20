aa98580e337579c026f717952fe56b452ffb8cb2
lucasmsdal <lucas.msdal@gmail.com>
2017-11-19 22:03:39 -0300

    Contém a função para buscar os links salvos no banco de dados

---
+++ banco-usuario.php
@@ -0,0 +1,11 @@
+﻿<?php 
+
+
+ function buscaUsuario ($conexao, $email, $senha){
+
+	$query = "select * from usuario where email='{$email}' and senha='{$senha}'";
+	$resultado = mysqli_query($conexao, $query);
+	$usuario = mysqli_fetch_assoc($resultado);
+	return $usuario;
+}
+
