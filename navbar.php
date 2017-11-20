3bf71c609873bfa4d95e731aab0212707b5b49e7
lucasmsdal <lucas.msdal@gmail.com>
2017-11-19 21:57:56 -0300

    Separação do tmeplate do bootstrap em cabeçalho, navbar e rodapé

--- navbar.php
+++ navbar.php
@@ -10,9 +10,13 @@
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav" "align center">
             <li><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
+            <li><a href="sair.php">Sair</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
         </div><!-- /.nav-collapse -->
       </div><!-- /.container -->
     </nav><!-- /.navbar -->
+<br/>
+<br/>
+<br/>
+<br/>
