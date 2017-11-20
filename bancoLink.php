011de42409eec260f3daf6d6de3d183fe48160bf
lucasmsdal <lucas.msdal@gmail.com>
2017-11-20 13:43:45 -0300

    Alteração no código onde é criado um array com os links salvos no banco

--- bancoLink.php
+++ bancoLink.php
@@ -6,7 +6,7 @@ function listaLink($conexao){
 	mysqli_set_charset($conexao, "utf8");
 	$cadastros = array();
 	$resultado = mysqli_query($conexao, "select * from cadastros");
-	while($pergunta = mysqli_fetch_assoc($resultado)){
+	while($cadastro = mysqli_fetch_assoc($resultado)){
 		array_push($cadastros, $cadastro);
 	}
 	return $cadastros;
