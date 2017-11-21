ffad9304328dd761a2b69b228dec0dc80a48a3be
lucasmsdal <lucas.msdal@gmail.com>
2017-11-21 14:59:21 -0300

    Adicionei a os botões para o usuário buscar e adicionar link, além de colocar as referências de cada botão

--- navbar.php
+++ navbar.php
@@ -1,17 +1,18 @@
 	<body>
-    <nav class="navbar navbar-fixed-top navbar-inverse">
+    <nav id="barra" class="navbar navbar-fixed-top navbar-inverse">
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
           </button>
-          <a class="navbar-brand" href="#">Cadastro de Links</a>
+          <a class="navbar-brand" href="index.php">Cadastro de Links</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav" "align center">
-            <li><a href="#">Home</a></li>
-            <li><a href="sair.php">Sair</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="busca.php">Busca</a></li>
+             <li><a href="linkForm.php">Cadastrar Link</a></li>
+              <li><a href="sair.php">Sair</a></li>
           </ul>
         </div><!-- /.nav-collapse -->
       </div><!-- /.container -->
